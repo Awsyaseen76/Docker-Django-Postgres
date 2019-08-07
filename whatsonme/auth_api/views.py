327b24fd081dd86064a407a5db14966d2b4cb5d6
Aws <Awsyaseen76@gmail.com>
2019-08-07 14:14:14 +0300

    change the list template to ListCreate now its CRUD

--- whatsonme/auth_api/views.py
+++ whatsonme/auth_api/views.py
@@ -2,11 +2,16 @@ from rest_framework import generics
 from .models import Auth
 from .serializers import AuthSerializer
 
-class AuthList(generics.ListAPIView): #Its ready template
+
+class AuthList(generics.ListCreateAPIView):  # Its ready template
    queryset = Auth.objects.all()
    serializer_class = AuthSerializer
 
 
-class AuthDetail(generics.RetrieveAPIView):
+# the power of generics is the templates
+# RetrieveAPIView -> allow just view one record
+# RetrieveUpdateDestroyAPIView -> view update delete
+
+class AuthDetail(generics.RetrieveUpdateDestroyAPIView):
    queryset = Auth.objects.all()
    serializer_class = AuthSerializer
